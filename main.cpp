933829e88e425aeb4ab3978415ad1713bc036a0e
pusheen1024 <rita26-10@mail.ru>
2025-12-04 17:00:03 +0400

    feat: add satellites

--- main.cpp
+++ main.cpp
@@ -11,13 +11,15 @@ const int HEIGHT = 600;
 const int BAR = 350;
 const Vector2 CENTER = {(WIDTH - BAR) / 2, HEIGHT / 2};
 
-int COEFF = 100; // скорость движения
+ld COEFF = 500; // скорость движения
 const ld EPS = 1e-9;
 
 const int SPACING = 1; // параметр для шрифта
 Font font;
 
-vector<const char*> paths = {"sun.png", "mercury.png", "venus.png", "earth.png", "mars.png", "jupiter.png", "saturn.png", "uranus.png", "neptune.png"};
+vector<const char*> paths = {"sun.png", "mercury.png", "venus.png", "earth.png", "mars.png", "jupiter.png", 
+							 "saturn.png", "uranus.png", "neptune.png", "moon.png", "phobos.png", "deimos.png",
+							 "io.png", "europe.png", "ganymede.png", "callisto.png"};
 vector<Image> images(paths.size());
 vector<Texture2D> textures(paths.size());
 vector<int> show_object(paths.size(), 1);
@@ -59,10 +61,8 @@ class CosmicObject {
 		int picture_id;
 		bool is_resized = 0;
 		bool show_text = 1;
-		bool show_planet = 1;
 		const char* name;
 		const char* type;
-		int font_size;
 
 	public:
 		float x;
@@ -80,8 +80,16 @@ class CosmicObject {
 			this->y = coords.y;
 		}
 
+		const char* getName() {
+			return name;
+		}
+
+		virtual const char* getType() {
+			return type;
+		}
+
 		const char* getInfo() {
-			return TextFormat("%s - %s с массой %Le кг.", name, type, mass); 
+			return TextFormat("%s - %s с массой %Le кг.", name, this->getType(), mass); 
 		}
 
 		Vector2 getCoords() {
@@ -107,7 +115,6 @@ class CosmicObject {
 			if (! is_resized) {
 				int scale = 1e2;
 				if (picture_id == 0) scale = 1e5;
-				if (picture_id > 4) scale = 1e3;
 				ImageResize(&image, diam / scale, diam / scale);
 				Texture2D texture = LoadTextureFromImage(image);
 				textures[picture_id] = texture;
@@ -136,30 +143,47 @@ class Sun: public CosmicObject {
 		}
 };
 
-class Planet: public CosmicObject {
+class RotatingObject: public CosmicObject {
 	protected:
-		ld a; // большая полуось
+		ld a; // большая полуось (в миллионах км)
 		ld e; // эксцентриситет
-		ld T; // период
+		ld T; // период (в земных годах)
 
 	public:
-		Planet(): CosmicObject() {
-			this->type = "планета";
-		}
-		ld getA() {return a; }
-		ld getB() {return a * sqrt(1 - e * e); }
+		RotatingObject() : CosmicObject() {}
+
+		virtual ld getA() {return a + image_width / 2; }
+		virtual ld getB() {return a * sqrt(1 - e * e) + image_height / 2; }
+
+		virtual ld center_x() {return 0; }
+		virtual ld center_y() {return 0; }
 
 		void updateCoords(ld t) {
 			ld M = 2 * PI * (t / (COEFF * T));
 			while (M > 2 * PI) M -= 2 * PI;	
 			auto E = kepler(M, e);
-			x = CENTER.x + a * (cos(E) - e);
-			y = CENTER.y + getB() * sin(E);
+			x = center_x() + getA() * (cos(E) - e);
+			y = center_y() + getB() * sin(E);
 		}
 
 		void drawOrbit() {
 			if (! show_object[picture_id]) return;
-			DrawEllipseLines(CENTER.x, CENTER.y, this->getA(), this->getB(), BLUE);
+			DrawEllipseLines(center_x(), center_y(), this->getA(), this->getB(), BLUE);
+		}
+};
+
+class Planet: public RotatingObject {
+	public:
+		Planet(): RotatingObject() {
+			this->type = "планета";
+		}
+
+		ld center_x() {
+			return CENTER.x;
+		}
+
+		ld center_y() {
+			return CENTER.y;
 		}
 };
 
@@ -267,6 +291,145 @@ class Neptune: public Planet {
 		}
 };
 
+class Satellite: public RotatingObject {
+	protected:
+		Planet *planet; // планета, вокруг которой вращается
+	
+	public:
+		Satellite() {}
+
+		Satellite(Planet* planet) : RotatingObject() {
+			this->planet = planet;
+			this->type = "спутник";
+		}
+		
+		ld getA() {
+			return a + planet->image_width / 2 + image_width / 2;
+		}
+
+		ld getB() {
+			return a * sqrt(1 - e * e) + planet->image_height / 2 + image_height / 2;
+		}
+
+		ld center_x() {
+			return planet->x;
+		}
+
+		ld center_y() {
+			return planet->y;
+		}
+
+		const char* getType() {
+			return TextFormat("%s планеты %s", type, planet->getName());
+		} 
+};
+
+class Moon: public Satellite {
+	public:
+		Moon(Planet* earth) : Satellite(earth) {
+			this->mass = 7.36e22;
+			this->picture_id = 9;
+			this->a = 384748.0 / 1e7;
+			this->e = 0.0549;
+			this->diam = 3474.8;
+			this->T = 27.32 / 365;
+			this->name = "Луна";
+		}
+};
+
+class Phobos: public Satellite {
+	public:
+		Phobos(Planet* mars) : Satellite(mars) {
+			this->mass = 1.072e16;
+			this->picture_id = 10;
+			this->a = 9377.0 / 1e7;
+			this->e = 0.015;
+			this->diam = 22.53;
+			this->T = 7.65 / 24 / 365;
+			this->name = "Фобос";
+		}
+};
+
+class Deimos: public Satellite {
+	public:
+		Deimos(Planet* mars) : Satellite(mars) {
+			this->mass = 1.48e15;
+			this->picture_id = 11;
+			this->a = 23458 / 1e7;
+			this->e = 0.0002;
+			this->diam = 12.4;
+			this->T = 1.2624 / 365;
+			this->name = "Деймос";
+		}
+};
+
+class Io: public Satellite {
+	public:
+		Io(Planet* jupiter) : Satellite(jupiter) {
+			this->mass = 8.9319e22;
+			this->picture_id = 12;
+			this->a = 421800 / 1e7;
+			this->e = 0.0041;
+			this->diam = 3643.2;
+			this->T = 1.769 / 365;
+			this->name = "Ио";
+		}
+};
+
+class Europe: public Satellite {
+	public:
+		Europe(Planet* jupiter) : Satellite(jupiter) {
+			this->mass = 4.8017e22;
+			this->picture_id = 13;
+			this->a = 671034 / 1e7;
+			this->e = 0.0094;
+			this->diam = 3121.6;
+			this->T = 3.55 / 365;
+			this->name = "Европа";
+		}
+};
+
+class Hanymede: public Satellite {
+	public:
+		Hanymede(Planet* jupiter) : Satellite(jupiter) {
+			this->mass = 1.4819e23;
+			this->picture_id = 14;
+			this->a = 1.07;
+			this->e = 0.0013;
+			this->diam = 5262;
+			this->T = 7.15 / 365;
+			this->name = "Ганимед";
+		}
+};
+
+class Callisto: public Satellite {
+	public:
+		Callisto(Planet* jupiter) : Satellite(jupiter) {
+			this->mass = 1.075e23;
+			this->picture_id = 15;
+			this->a = 1.882;
+			this->e = 0.0074;
+			this->diam = 4820.6;
+			this->T = 16.7 / 365;
+			this->name = "Каллисто";
+		}
+};
+
+class Comet: public CosmicObject {
+	Comet(ld mass, ld velocity) : CosmicObject() {
+		this->x = 0;
+		this->y = 0;
+		this->mass = mass;
+		this->picture_id = 16;
+		this->diam = this->mass / 100;
+		this->name = "Комета";
+	}
+
+	void updateCoords() {
+	}
+	
+};
+
 int main() {
     InitWindow(WIDTH, HEIGHT, "Компьютерная модель Солнечной системы");
     SetTargetFPS(60); 
@@ -300,9 +463,18 @@ int main() {
 	Saturn saturn = Saturn();
 	Uranus uranus = Uranus();
 	Neptune neptune = Neptune();
+	Moon moon = Moon(&earth);
+	Phobos phobos = Phobos(&mars);
+	Deimos deimos = Deimos(&mars);
+	Io io = Io(&jupiter);
+	Europe europe = Europe(&jupiter);
+	Hanymede hanymede = Hanymede(&jupiter);
+	Callisto callisto = Callisto(&jupiter);
 	vector<Planet*> planets = {&mercury, &venus, &earth, &mars, &jupiter, &saturn, &uranus, &neptune};
+	vector<Satellite*> satellites = {&moon, &phobos, &deimos, &io, &europe, &hanymede, &callisto};
 	vector<CosmicObject*> objects = {&sun};
 	for (auto planet : planets) objects.push_back(planet);
+	for (auto satellite : satellites) objects.push_back(satellite);
 	int n = objects.size();
 	vector<Label> labels(n);
 	vector<CheckBox> checkboxes(n);
@@ -310,17 +482,14 @@ int main() {
 	vector<Color> colors = {RED, GREEN};
 	int x = 180;
 	for (int i = 0; i < n; i++) {
-		auto info = sun.getInfo();
-		if (i > 0) info = planets[i - 1]->getInfo();
-		labels[i] = Label("", WIDTH - BAR, x, font, 20, RED);
-		labels[i].setText(info);
+		auto info = objects[i]->getInfo();
+		labels[i] = Label(info, WIDTH - BAR, x, font, 20, RED);
 		checkboxes[i] = CheckBox(texts, WIDTH - BAR + 100, x, font, 20, BLACK, colors);
 		x += 20;
 	}
 	ld t = 0;
-	bool coeff_changed = 0;
     while (!WindowShouldClose()) {
-	    BeginDrawing();
+		BeginDrawing();
         ClearBackground(BLACK);
 		DrawRectangle(WIDTH - BAR, 0, BAR, HEIGHT, WHITE);
 		label_mass.render();
@@ -342,17 +511,10 @@ int main() {
 			for (int i = 0; i < n; i++) {
 				if (checkboxes[i].toggle()) show_object[i] ^= 1;
 			}
-			if (inc.click() && COEFF < 300) {
-				COEFF++;
-				coeff_changed = 1;
-			}
-			else if (dec.click() && COEFF > 10) {
-				COEFF--;
-				coeff_changed = 1;
-			}
-			else coeff_changed = 0;
+			if (inc.click()) COEFF *= 2;
+			else if (dec.click()) COEFF /= 2;
 		}
-		else coeff_changed = 0;
+		BeginMode2D(camera);
 		if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
 			Vector2 delta = GetMouseDelta();
 			delta *= (-1 / camera.zoom);
@@ -372,7 +534,6 @@ int main() {
 		else if (input_velocity.isActive()) {
 			input_velocity.handleKeyboard();
 		}
-		BeginMode2D(camera);
 		if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {			
 			Vector2 real_pos = GetScreenToWorld2D(GetMousePosition(), camera);
 			sun.showText(real_pos);
@@ -382,15 +543,20 @@ int main() {
 		}
 		sun.render();
 		for (auto planet : planets) {
-			if (! coeff_changed) (*planet).updateCoords(t);
-		}
-		for (auto planet : planets) {
+			(*planet).updateCoords(t);
 			(*planet).drawOrbit();
-			(*planet).render();
+		}
+		for (auto satellite : satellites) {
+			(*satellite).updateCoords(t);
+			(*satellite).drawOrbit();
+			(*satellite).render();
+		}
+		for (auto obj : objects) {
+			(*obj).render();
 		}
 		EndMode2D();
 		EndDrawing();
-		if (! coeff_changed) t += 0.05;
+		t += 0.05;
 	}
     CloseWindow(); 
     return 0;
